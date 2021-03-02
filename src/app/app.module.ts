12b0f35274509d11ce139c5556923242f2a1e587
Natalie Enkin <natalie@enkin.co.il>
2021-03-02 22:05:44 +0200

    updating current app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http"
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http"
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from "@angular/material/toolbar"
+import { MatIconModule } from "@angular/material/icon";
+import { MatGridListModule } from '@angular/material/grid-list';
 
 
 import { AppComponent } from './app.component';
-import {ParticipantComponent} from "./components/participant/participant.component";
-import { ParticipantsComponent } from './components/participants/participants.component'
+import { ParticipantComponent } from "./components/participant/participant.component";
+import { ParticipantsComponent } from './components/participants/participants.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
-    AppComponent,ParticipantComponent, ParticipantsComponent,
+    AppComponent,
+    ParticipantComponent,
+    ParticipantsComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule, 
-    HttpClientModule
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/participants/participants.component.ts
+++ src/app/components/participants/participants.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-participants',
-  templateUrl: './participants.component.html',
+  templateUrl: "./participants.component.html",
   styleUrls: ['./participants.component.css']
 })
 export class ParticipantsComponent implements OnInit {
